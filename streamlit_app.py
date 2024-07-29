ad016b6324ec8bf1b51c8fbe358824649dfa1f38
Danish <danishajaib@outlook.com>
2024-07-29 18:31:12 +1000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,11 +138,12 @@ st.markdown('''
 ## Projects
 ''')
 tab1, tab2, tab3, tab4, tab5 = st.tabs([
-    'Writing', 
-    'Visualization', 
-    'Flutter', 
     'Data Analysis & ML', 
-    'Python'
+    'Data Visualization',
+    'Python Scripting'
+    'Research & Writing', 
+    'App Development',  
+    
 ])
 
 
@@ -154,7 +155,7 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs([
 #     sac.TabsItem(label='Python', icon='code'),
 # ], align='start')
 
-with tab1:
+with tab4:
   txt('#### A Survey of Cross-Domain Recommender Systems','')
   st.markdown('''
   This work is a literature review and delves into overcoming traditional collaborative filtering challenges using advanced techniques like transfer learning and neural collaborative filtering. It highlights the transformative potential of these systems across diverse industries, from e-commerce to healthcare. My journey through this project enhanced my understanding of data science intricacies and future tech trends, contributing valuable insights into the evolution of user experience in digital domains.
@@ -169,7 +170,7 @@ with tab1:
   txt5('**Skills**', '`Communication`, `Market Data Analysis`, `Market Research`, `Team Collaboration`')
   st.link_button(label='Report', url='https://www.linkedin.com/in/danish-ajaib-865528107/details/projects/1704617545125/single-media-viewer?type=DOCUMENT&profileId=ACoAABrzyM0BjMYrqY8KqDQ1UnHu_HgkagWHQ6A&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_projects_details%3BDDyIlEzdTFGKgFdJDz3dRw%3D%3D')
 
-with tab2:
+with tab1:
   txt('#### Predicting Employee Churn','')
   st.markdown('''
   Analysing employee churn data and building a model to predict employee churn..
@@ -197,14 +198,6 @@ with tab2:
   ''')
   txt5('**Skills**', '`Pandas`, `Python`, `Web Scraping`, `EDA`, `Model Building`, `Data Visualization`')
   st.link_button(label='Notebook', url='https://github.com/DanishAjaib/Amazon-Sentiment-Analysis')
-  
-with tab4:
-  txt('#### Market Analysis and Strategy','')
-  st.markdown('''
-  In this project during my internship, I was part of a team of four tasked with developing a market analysis and marketing plan for an emerging oat milk brand. We conducted in-depth research on market trends and competitor strategies, focusing on plant-based dairy alternatives. Our approach included frequent collaborative team meetings and strategic discussions with the CEO, leading to a tailored marketing strategy. The strategy emphasized health, sustainability, and allergen-free benefits, targeting diverse consumer demographics. This project honed my skills in teamwork, strategic planning, and high-level corporate communication.
-  ''')
-  txt5('**Skills**', '`Communication`, `Market Data Analysis`, `Market Research`, `Team Collaboration`')
-  st.link_button(label='Report', url='https://www.linkedin.com/in/danish-ajaib-865528107/details/projects/1704617545125/single-media-viewer?type=DOCUMENT&profileId=ACoAABrzyM0BjMYrqY8KqDQ1UnHu_HgkagWHQ6A&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_projects_details%3BDDyIlEzdTFGKgFdJDz3dRw%3D%3D')
 
  
 #####################

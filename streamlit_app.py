9cabb935b08f7c4c9ac939c1c73273a16879a723
Danish <danishajaib@outlook.com>
2024-07-29 16:26:38 +1000

    added some projects

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,11 +135,29 @@ st.markdown('''
 ## Projects
 ''')
 
-txt('**A Survey of Cross-Domain Recommender Systems**','Literature Review')
+txt('**A Survey of Cross-Domain Recommender Systems**','Literature Review/Writing')
 st.markdown('''
 This work is a literature review and delves into overcoming traditional collaborative filtering challenges using advanced techniques like transfer learning and neural collaborative filtering. It highlights the transformative potential of these systems across diverse industries, from e-commerce to healthcare. My journey through this project enhanced my understanding of data science intricacies and future tech trends, contributing valuable insights into the evolution of user experience in digital domains.
 ''')
 txt5('**Skills**', '`Technial Writing`, `Research Skills`, `Analytical Skills`, `Communication`, `Synthesis of Information`')
+
+txt('**Amazon Sentiment Analysis**','Analysis and Model Building')
+st.markdown('''
+A university project focusing on Web Scraping, Data Cleaning, Data Analsis and finally building a Model for sentiment prediction.
+''')
+txt5('**Skills**', '`Pandas`, `Python`, `Web Scraping`, `EDA`, `Model Building`, `Data Visualization`')
+
+txt('**Australia Rain Visualization and Forecast**','Analysis and Model Building')
+st.markdown('''
+A hoppy project analysing Australia rain patterns and building a model for predicting next day rain.
+''')
+txt5('**Skills**', '`Pandas`, `Python`, `Web Scraping`, `EDA`, `Model Building`, `Data Visualization`')
+
+txt('**Market Analysis and Strategy for New oat Milk Company**','Research')
+st.markdown('''
+In this project during my internship, I was part of a team of four tasked with developing a market analysis and marketing plan for an emerging oat milk brand. We conducted in-depth research on market trends and competitor strategies, focusing on plant-based dairy alternatives. Our approach included frequent collaborative team meetings and strategic discussions with the CEO, leading to a tailored marketing strategy. The strategy emphasized health, sustainability, and allergen-free benefits, targeting diverse consumer demographics. This project honed my skills in teamwork, strategic planning, and high-level corporate communication.
+''')
+txt5('**Skills**', '`Communication`, `Market Data Analysis`, `Market Research`, `Team Collaboration`')
 
 #####################
 st.markdown('''

faee5e7d0975c8a41e4cc07d2491370eb232d2a2
Danish <danishajaib@outlook.com>
2024-08-07 17:32:03 +1000

    updates

--- views/british_airways_bookings_analysis.py
+++ views/british_airways_bookings_analysis.py
@@ -86,23 +86,25 @@ def plot_correlation_matrix(data):
 
 st.title('British Airways bookings analysis')
 st.markdown('In this project we will analyse customer bookings data for Brith Airways , prepare it for training and finally train a model predict whether a customer completes a booking or not.')
-df = pd.read_csv('data/british_airways_bookings.csv', encoding="ISO-8859-1", index_col=0, header=0)
+df = pd.read_csv('data/british_airways_bookings.csv', encoding="ISO-8859-1", header=0).reset_index(drop=True)
 
 st.dataframe(df.head())
 
 st.markdown('First we try to understand what the dataset looks like')
 st.dataframe(get_info_df(df), width=920)
+
+
 st.markdown(
     '''
 We can see that the dataset has a total of `13` columns, 50K rows , `5 integer`, `1 float` and 
 `5 categorical` features. Next, lets see the data summary for further exploration.
 '''
 )
-st.dataframe(df.describe())
+
 st.markdown(
     '''
 The table above gives us an overview of the data. We can see that all columns have 50K Non Null values which means
-there is not missing data. The other metrics give us useful insights about each feature, 
+there is no missing data. The other metrics give us useful insights about each feature, 
 e.g total number of `non-null` rows, `average value`, `standard deviation`, `minimum value`, `maximum value`, 
 and the `percentiles` of the distribution. Next, we need to check how the data in each variable is distributed to see if any variables are skewed or not.
 '''
@@ -114,6 +116,12 @@ st.title("Data Exploration and Preparation")
 
 # Select numerical columns
 numerical_columns = df.select_dtypes(include=['int64', 'float64']).nunique().loc[lambda x: x > 2].index.tolist()
+
+# Check if 'num_passengers' is in the list of numerical columns
+if 'num_passengers' not in numerical_columns:
+    st.text('Error: num_passengers column not found in numerical columns')
+else:
+    st.text('num_passengers column found in numerical columns')
 
 create_histogram_grid(features_num=numerical_columns, num_cols=3, df=df)
 
@@ -141,9 +149,12 @@ categorical_features = ['sales_channel', 'trip_type', 'flight_day', 'route', 'bo
 for col in continuous_features_transformed:
     df_transformed[col] = np.log1p(df[col])
 
+st.title("Feature Scaling")
 
-scaler = StandardScaler()
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
 df_transformed[continuous_features_transformed] = scaler.fit_transform(df_transformed[continuous_features_transformed])
+pickle.dump(scaler, open('models/british_airways_bookings_scaler.pkl', 'wb'))
 create_histogram_grid(features_num=continuous_features_transformed, num_cols=3, df=df_transformed)
 st.markdown(
     '''
@@ -151,9 +162,10 @@ st.markdown(
     means most of the outliers are gone. Now we need to apply one-hot-encoding to convert the categorical features.
 '''
 )
+st.title("Encoding categorical features")
 df_encoded = pd.get_dummies(df_transformed, columns = categorical_features,)
 st.dataframe(df_encoded.head())
-
+all_features = df_encoded.columns.tolist()
 st.markdown(
     f'''
    Our data is now clean but before we proceed with training a model we first need to 
@@ -168,6 +180,76 @@ st.markdown(
    There doesn't seem to be any strong correlations among features. Now we check correlation between categorical variables using the chi2 contigency from scipy.
 '''
 )
+
+@st.cache_data(persist='disk')
+def predict_booking_complete(num_passengers,sales_channel,trip_type,purchase_lead,length_of_stay,flight_hour,flight_day,route,booking_origin,wants_extra_baggage,wants_preferred_seat,wants_in_flight_meals,flight_duration):
+    
+    # Load the model
+    model = joblib.load('models/british_airways_bookings_random_forest.pkl')
+    
+
+    # Create a dataframe with the new data
+    new_data = pd.DataFrame({
+        'num_passengers': [num_passengers],
+        'sales_channel': [sales_channel],
+        'trip_type': [trip_type],
+        'purchase_lead': [purchase_lead],
+        'length_of_stay': [length_of_stay],
+        'flight_hour': [flight_hour],
+        'flight_day': [flight_day],
+        'route': [route],
+        'booking_origin': [booking_origin],
+        'wants_extra_baggage': [wants_extra_baggage],
+        'wants_preferred_seat': [wants_preferred_seat],
+        'wants_in_flight_meals': [wants_in_flight_meals],
+        'flight_duration': [flight_duration]
+    })
+
+    #all all_features to the new data
+
+
+    st.write("New data before preprocessing:")
+    st.dataframe(new_data)
+    
+    # Preprocess the data
+    num_columns = ['num_passengers', 'purchase_lead', 'length_of_stay', 'flight_hour', 'flight_duration']
+    cat_columns = ['sales_channel', 'trip_type', 'flight_day', 'route', 'booking_origin']
+
+
+    
+    # Log transform the numerical columns
+    new_data[num_columns] = new_data[num_columns].apply(lambda x: np.log1p(x.clip(lower=0) + 1))
+    
+    st.text("After log transformation:")
+    st.dataframe(new_data.head())
+    
+    scaler = joblib.load('models/british_airways_bookings_scaler.pkl', )
+    new_data[num_columns] = scaler.transform(new_data[num_columns])
+    
+    st.text("After scaling:")
+    st.dataframe(new_data.head())
+    
+
+    new_data = pd.get_dummies(new_data, columns=cat_columns)
+    
+    st.text("After one-hot encoding:")
+    st.dataframe(new_data)
+    
+    #remove booking_complete from all_features
+    all_features.remove('booking_complete')
+    for col in all_features:
+        if col not in new_data.columns and col != 'booking_complete':
+            new_data[col] = False
+    
+    new_data = new_data[all_features]
+
+    # Ensure the new data has the same columns as the training data
+    # Note: You might need to align columns with the training data here
+    st.text("Prepared Data:")
+    st.dataframe(new_data.head(10))
+    # Make a prediction
+    probablity = model.predict_proba(new_data)
+    return probablity
 
 def categorical_corr_matrix(df, cat_vars, alpha=0.05):
     """
@@ -216,58 +298,181 @@ X = df_encoded.drop(columns=['booking_complete'], axis=1)
 y = df_encoded['booking_complete']
 
 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-smote = SMOTE(random_state=42)
-X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
-
-st.subheader('Logistic Regression')
+st.subheader('Random Forest')
 import joblib
 
 @st.cache_resource
-def train_log_reg():
+def train_model():
+    from sklearn.ensemble import RandomForestClassifier
+    _model=RandomForestClassifier(n_estimators=1000)
+    _model.fit(X_train,y_train)
+    joblib.dump(_model, 'models/british_airways_bookings_random_forest.pkl')
 
-    result = ''
-    model = LogisticRegression(max_iter=1000)
-    param_grid = {'C': [0.1, 1, 10, 100], 'penalty': ['l2']}
-    grid_search = GridSearchCV(estimator = model,  param_grid = param_grid, scoring = 'accuracy', cv = 5, verbose=0)
-    grid_search.fit(X_train_res, y_train_res)
-    result = grid_search.best_estimator_
-    joblib.dump(result, 'models/british_airways_bookings_log_reg.pkl')
+@st.cache_data(persist='disk')
+def get_feature_importance(_model, X):
+    #plot feature importance
+    feature_importance = _model.feature_importances_
+    feat_importances = pd.Series(feature_importance, index=X_train.columns)
+    feat_importances = feat_importances.nlargest(10).reset_index()
+    feat_importances.columns = ['Feature', 'Importance']
+
+    return feat_importances
+
+def plot_feature_importance(feat_importances):
+    chart = alt.Chart(feat_importances).mark_bar().encode(
+    x='Importance',
+    y=alt.Y('Feature', sort='-x')
+    ).properties(
+        title='Feature Importance'
+    )
+    # Display the chart using Streamlit
+    st.altair_chart(chart, use_container_width=True)
+@st.cache_data(persist='disk')
+def get_confusion_matrix(_model, X_test, y_test):
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, _model.predict(X_test))
+    return cm
+
+@st.cache_data(persist='disk')
+def plot_confusion_matrix(confusion_matrix):
+    cm_df = pd.DataFrame(confusion_matrix, index=['Actual Negative', 'Actual Positive'], columns=['Predicted Negative', 'Predicted Positive'])
+    st.dataframe(cm_df)
+
+@st.cache_data(persist='disk')
+def get_classification_report(_model, X_test, y_test):
+    from sklearn.metrics import classification_report
+    report = classification_report(y_test, _model.predict(X_test), output_dict=True)
+    return report
+
+@st.cache_data(persist='disk')
+def plot_classification_report(report):
+    report_df = pd.DataFrame(report).transpose()
+    st.dataframe(report_df)
+
+# log_reg = joblib.load('models/british_airways_bookings_log_reg.pkl')
+#check if model already exists
+import os
+if not os.path.exists('models/british_airways_bookings_random_forest.pkl'):
+    train_model()
+else:
+    _model = joblib.load('models/british_airways_bookings_random_forest.pkl')
+
+st.header('Model Evaluation')
+
+st.subheader('Feature Importance')
+feature_importance = get_feature_importance(_model, X_train)
+# Create the Altair chart
+plot_feature_importance(feature_importance)
+
+confusion_matrix = get_confusion_matrix(_model, X_test, y_test)
+plot_confusion_matrix(confusion_matrix)
+
+classification_report = get_classification_report(_model, X_test, y_test)
+plot_classification_report(classification_report)
 
 
-log_reg = joblib.load('models/british_airways_bookings_log_reg.pkl')
+st.header('Conclusion')
 
-#train_log_reg()
-#Random Forest
-# from sklearn.ensemble import RandomForestClassifier
-# forest_reg = RandomForestClassifier()
-# forest_reg.fit(X_train, y_train)
-# #XGB
-# xgb_model = xgb.XGBClassifier(random_state=42)
-# xgb_model.fit(X_train, y_train)
+st.markdown(
+    '''
+    The model demonstrates a strong performance with an overall accuracy of 85.18%. The precision and recall for the negative class (0) are notably high at 86.68% and 97.61%, respectively, indicating that the model is highly effective at correctly identifying negative instances. However, the precision and recall for the positive class (1) are lower at 49.42% and 13.49%, respectively, suggesting that the model struggles to accurately predict positive instances.
+The confusion matrix further illustrates this, with 12,478 true negatives and 299 true positives, but also 306 false negatives and 1,917 false positives. This imbalance highlights the challenge in predicting the positive class accurately.
+The feature importance analysis reveals that 'purchase_lead', 'flight_hour', and 'length of stay' are the top three most influential features in the model. These features significantly contribute to the model's predictions and should be considered key factors in understanding booking behaviors.
+Overall, while the model performs well in predicting the negative class, there is room for improvement in predicting the positive class. Future efforts could focus on addressing this imbalance to enhance the model's predictive capabilities for both classes
+    '''
+)
 
-def evaluate_model(model):
+st.title('Test the model')
 
-    test_predictions = model.predict(X_test)
-    train_predictions = model.predict(X_train)
-    if hasattr(model, "predict_proba"):
-        test_probs = model.predict_proba(X_test)[:, 1]  # Get probabilities for the positive class
-        auc = roc_auc_score(y_test, test_probs)
-    else:
-        auc = "N/A"  # ROC AUC requires probability scores
+# Define the options for dropdowns
+sales_channel_options = df['sales_channel'].unique().tolist()
+trip_type_options = df['trip_type'].unique().tolist()
+flight_day_options = df['flight_day'].unique().tolist()
+route_options = df['route'].unique().tolist()
+booking_origin_options = df['booking_origin'].unique().tolist()
 
-    auc = roc_auc_score(y_test, test_predictions)
-    accuracy = accuracy_score(y_test, test_predictions)
-    precision = precision_score(y_test, test_predictions)
-    recall = recall_score(y_test, test_predictions)
-    
-    return {
-        'auc':auc,
-        'accuracy': accuracy,
-        'precision': precision,
-        'recall': recall
-    }
 
-st.text(evaluate_model(model=log_reg))
+num_passengers = st.number_input(
+    'Number of Passengers', 
+    min_value=int(df['num_passengers'].min()), 
+    max_value=int(df['num_passengers'].max()), 
+    value=1
+)
+sales_channel = st.selectbox('Sales Channel', sales_channel_options)
+trip_type = st.selectbox('Trip Type', trip_type_options)
+purchase_lead = st.number_input(
+    'Purchase Lead (days)', 
+    min_value=int(df['purchase_lead'].min()), 
+    max_value=int(df['purchase_lead'].max()), 
+    value=5, 
+    step=1
+)
+length_of_stay = st.number_input(
+    'Length of Stay (days)', 
+    min_value=int(df['length_of_stay'].min()), 
+    max_value=int(df['length_of_stay'].max()), 
+    value=3,
+)
+flight_hour = st.number_input('Flight Hour', min_value=0, max_value=23, value=0)
+flight_day = st.selectbox('Flight Day', flight_day_options)
+route = st.selectbox('Route', route_options)
+booking_origin = st.selectbox('Booking Origin', booking_origin_options)
+wants_extra_baggage = st.checkbox('Wants Extra Baggage')
+wants_preferred_seat = st.checkbox('Wants Preferred Seat')
+wants_in_flight_meals = st.checkbox('Wants In-Flight Meals')
+flight_duration = st.number_input(
+    'Flight Duration (hours)', 
+    min_value=float(df['flight_duration'].min()), 
+    max_value=float(df['flight_duration'].max()), 
+    value=5.0
+)
+st.subheader('Booking Completion Prediction')
+# Display the input values
+st.subheader('Input Values')
+st.write({
+    'num_passengers': num_passengers,
+    'sales_channel': sales_channel,
+    'trip_type': trip_type,
+    'purchase_lead': purchase_lead,
+    'length_of_stay': length_of_stay,
+    'flight_hour': flight_hour,
+    'flight_day': flight_day,
+    'route': route,
+    'booking_origin': booking_origin,
+    'wants_extra_baggage': wants_extra_baggage,
+    'wants_preferred_seat': wants_preferred_seat,
+    'wants_in_flight_meals': wants_in_flight_meals,
+    'flight_duration': flight_duration,
+})
+
+prediction = predict_booking_complete(num_passengers, sales_channel, trip_type, purchase_lead, length_of_stay, flight_hour, flight_day, route, booking_origin, wants_extra_baggage, wants_preferred_seat, wants_in_flight_meals, flight_duration)
+st.header('Prediction')
+
+@st.cache_resource
+def show_donut_chart(prediction_probabilities):
+    color_red, color_green = '#FF204E', '#BED754'
+
+    # Create a DataFrame with the prediction probabilities
+    df = pd.DataFrame({
+        'Class': ['Not Complete', 'Complete'],
+        'Probability': [1 - prediction_probabilities[0][0], prediction_probabilities[0][0]],
+        'Color': [color_red, color_green]
+    })
+
+    # Create a donut chart using Altair
+    chart = alt.Chart(df).mark_arc(innerRadius=50).encode(
+        theta=alt.Theta(field='Probability', type='quantitative'),
+        color=alt.Color(field='Color', type='nominal', scale=None),
+        tooltip=['Class', 'Probability']
+    ).properties(
+        width=400,
+        height=400
+    )
+
+    # Display the chart in Streamlit
+    st.altair_chart(chart, use_container_width=True)
+
+show_donut_chart(prediction)
+# st.text(evaluate_model(model=log_reg))
